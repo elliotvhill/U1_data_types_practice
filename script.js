c8b436f0e3351465d37a268c6e3ea4402059f8df
Elliot <ellthrowaway2@icloud.com>
2023-05-09 15:07:35 -0400

    completed Arrays prompt #3

--- script.js
+++ script.js
@@ -87,9 +87,9 @@ console.log(`Array 3: `, array3[1][1][1][0]) */
  * Use the length of the array below to retrieve the second to last item.
  */
 
-let array4 = ['a', 'b', 'c', 'd', 'e'];
+/* let array4 = ['a', 'b', 'c', 'd', 'e'];
 
-console.log(`The second to last item in the array is `, array4[(array4.length - 2)])
+console.log(`The second to last item in the array is `, array4[(array4.length - 2)]) */
 
 /*
  * Prompt 3:
@@ -104,10 +104,19 @@ let cathleen = ['Cathleen', 186, 'New York'];
 
 // Cathleen decides that Thom can't be named "Thom" anymore. Remove "Thom" from
 // the thom array and replace it with "Tom".
+thom.splice(0, 1, 'Tom')
+// console.log(thom)
 
 // Karolin just had her birthday; change Karolin's array to reflect her being
 // a year older.
+karolin.splice(1, 1, 17)
+// console.log(karolin)
 
 // Change Cathleen's hometown from New York to "Gotham City".
+cathleen.splice(2, 1, 'Gotham City')
+// console.log(cathleen)
 
 // Remove "Pittsburgh" from Kristyn's array and add "Oakland".
+kristyn.pop()
+kristyn.push('Oakland')
+console.log(kristyn)
